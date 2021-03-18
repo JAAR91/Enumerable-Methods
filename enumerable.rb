412d3c35d980cc55d2ae3bfe08b91dcc689873c1
carlos-osorio-developer <osorio.uis@gmail.com>
2021-03-18 09:37:50 -0500

    Add my_all method

--- enumerable.rb
+++ enumerable.rb
@@ -1,25 +1,32 @@
-def my_each(array)
-  i = 0
-  while (i - array.length).negative?
-    yield(array[i])
-    i += 1
+module Enumerable
+  def my_each(array)
+    i = 0
+    while (i - array.length).negative?
+      yield(array[i])
+      i += 1
+    end
   end
-end
 
-def my_each_with_index(array)
-  array.length.times do |i|
-    yield(array[i], i)
+  def my_each_with_index(array)
+    array.length.times do |i|
+      yield(array[i], i)
+    end
   end
-end
 
-def my_select(array)
-  newarray = []
-  array.each do |item|
-    newarray.push(item) if yield(item)
+  def my_select(array)
+    newarray = []
+    array.each do |item|
+      newarray.push(item) if yield(item)
+    end
+    newarray
   end
-  newarray
+
+  def my_all(array)   
+    output=true 
+  array.my_each(array){|item| output=false if !yield(item)}      
+  return output  
+  end
 end
 
 array = %w[sharon leo leila brian aaron]
-array2 = [2, 3, 3]
-puts my_select(array) { |friend| friend.length > 6 }
+puts array.my_all(array) {|item| item.length<10}

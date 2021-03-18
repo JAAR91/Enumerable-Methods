2ee5f21ee425cb5c3988a8d331acee34d4e14528
carlos-osorio-developer <osorio.uis@gmail.com>
2021-03-18 14:36:15 -0500

    Edit my_map method

--- enumerable.rb
+++ enumerable.rb
@@ -1,14 +1,14 @@
 module Enumerable
   def my_each()
     i = 0
-    while (i - self.length).negative?
+    while (i - length).negative?
       yield(self[i])
       i += 1
     end
   end
 
   def my_each_with_index()
-    self.length.times do |i|
+    length.times do |i|
       yield(self[i], i)
     end
   end
@@ -21,48 +21,47 @@ module Enumerable
     newarray
   end
 
-  def my_all?()   
-    output = true 
-    self.my_each{|item| output = false if !yield(item)}      
-    output  
+  def my_all?()
+    output = true
+    my_each { |item| output = false unless yield(item) }
+    output
   end
 
   def my_any?()
     output = false
-    self.my_each{|item| output = true if yield(item)}
+    my_each { |item| output = true if yield(item) }
     output
   end
 
   def my_none?()
     output = true
-    self.my_each{|item| output = false if yield(item)}
+    my_each { |item| output = false if yield(item) }
     output
-  end 
+  end
 
   def my_count()
     count = 0
-    self.my_each{|item| count += 1 if yield(item)}
+    my_each { |item| count += 1 if yield(item) }
     count
   end
 
   def my_map(proc = nil)
     newarray = []
-    self.my_each{|item| newarray.push(proc[item])}
+    if proc.nil?
+      my_each { |item| newarray.push(yield(item)) }
+    else
+      my_each { |item| newarray.push(proc[item]) }
+    end
     newarray
   end
 
   def my_inject()
-    result=self[0]
-    self.my_each{|item| result=yield(result,item)}
+    result = self[0]
+    my_each { |item| result = yield(result, item) }
     result
   end
 
   def multiply_els()
-    self.my_inject{ |factora, factorb| factora * factorb }
+    my_inject { |factora, factorb| factora * factorb }
   end
-
 end
-
-array = [1, 2, 3, 4]
-multiply = Proc.new{|num| num * 2}
-p array.my_map(multiply)
